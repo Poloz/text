d4cedfae413bc6820b19cacc70b10a6e16040113
Poloz <underpoloz@gmail.com>
2015-09-17 18:06:47 +0300

    java-1

---
+++ prj/java/src/com/poloz/Main.java
@@ -0,0 +1,14 @@
+package com.poloz;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}

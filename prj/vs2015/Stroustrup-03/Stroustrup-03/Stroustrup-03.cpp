0d4584ee32eb0dbae5026208e949b98bd8748df2
Poloz <underpoloz@gmail.com>
2016-04-21 01:53:22 +0300

    Stroustrup-03
    initial 03 proj

---
+++ prj/vs2015/Stroustrup-03/Stroustrup-03/Stroustrup-03.cpp
@@ -0,0 +1,13 @@
+// Stroustrup-03.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Gotcha!\n";
+	keep_window_open();
+    return 0;
+}
+

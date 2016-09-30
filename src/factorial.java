7cb42e110cee645f7554ef544d88bb6990974f28
Alexander Baranov <albar@joybits.org>
2016-09-30 15:52:23 +0300

    java

---
+++ src/factorial.java
@@ -0,0 +1,17 @@
+/**
+ * Merges two given sorted arrays into one
+ * 1). Используйте StringBuilder и его методы append() и toString() для скорости выполнения.
+ *
+ * 2). Используйте методы String:  startsWith() для поиска реплик данной роли в элементе 
+ * textLines и replaceFirst() для устранения имени роли в тексте реплики, так как есть подозрение, 
+ * что конструкция "Роль:"  может встречаться ДВАЖДЫ внутри элемента textLines 
+ * (вывод из фразы: "... имя персонажа может встречаться в строке более одного раза, в том числе с двоеточием;")
+ */
+
+private String printTextPerRole(String[] roles, String[] textLines) {
+	String out[] = new String[]
+	for (int i=1; i<=textLines.length; i++){
+
+	}
+    return "";
+}

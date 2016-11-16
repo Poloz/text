3d6f89ec7382f2f5b73a91600c9971d561a8f1ea
Alexander Baranov <baranov.alex.main@gmail.com>
2016-11-17 01:59:09 +0300

    No Return

---
+++ libgdx/noreturn/android/src/com/poloz/noreturn/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.poloz.noreturn;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.poloz.noreturn.NoReturnGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NoReturnGame(), config);
+	}
+}
---
+++ libgdx/noreturn/desktop/src/com/poloz/noreturn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.poloz.noreturn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.poloz.noreturn.NoReturnGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NoReturnGame(), config);
+	}
+}

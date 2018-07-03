932622fa6666d969ad619b3d0119eb86d62a39b8
Alexander Baranov <albar@joybits.org>
2018-07-03 04:53:31 +0300

    Table

--- arx/2013-02/2013-02/2013-02.cpp
+++ arx/2013-02/2013-02/2013-02.cpp
@@ -23,18 +23,12 @@ void selectBlocks()
 	ads_name ssname;
 	struct resbuf blockFilter;//, insertFilter;
 
-	ACHAR insertName[15] = ACRX_T("Arm_zone_v001");
-	//ACHAR insertName[15] = ACRX_T("TestBlock01");
-	
-	//insertFilter.restype = 2;	// Entity name
-	//insertFilter.resval.rstring = insertName;
-	//insertFilter.rbnext = NULL; // No other properties
+	const ACHAR *prefix = ACRX_T("Arm_");
 
 	blockFilter.restype = 0; // Entity type
 	blockFilter.resval.rstring = ACRX_T("INSERT");
-	blockFilter.rbnext = NULL; //&insertFilter;
-
-	acutPrintf(ACRX_T("\nPlease select %s blocks."), insertName);
+	blockFilter.rbnext = NULL;
+	acutPrintf(ACRX_T("\nPlease select blocks."));
 	
 	if (acedSSGet(NULL, NULL, NULL, &blockFilter, ssname) != RTNORM )
 	{
@@ -43,15 +37,13 @@ void selectBlocks()
 		return;
 	}
 	
-	long length = 0;
-	
+	long length = 0;	
 	if (acedSSLength(ssname, &length) != RTNORM)
 	{
 		acutPrintf(ACRX_T("\nNo blocks selectes\n"));
 		acedSSFree(ssname);
 		return;
-	}
-	
+	}	
 	ads_name ent;
 	long counter = 0;
 	AcDbObjectId obId = AcDbObjectId::kNull;
@@ -59,11 +51,8 @@ void selectBlocks()
 	{
 		if (acedSSName(ssname, i, ent) != RTNORM) {acutPrintf(ACRX_T("\nCannot get entity from selection.\n")); continue;};
 		if (acdbGetObjectId(obId, ent) != Acad::eOk) {acutPrintf(ACRX_T("\nCannot get ObjectId from entity.\n")); continue;};
-		//AcDbEntity* pEnt = NULL;
-		//if (acdbOpenAcDbEntity(pEnt, obId, AcDb::kForRead) != Acad::eOk) {acutPrintf(ACRX_T("\nCannot open for read.\n")); continue;};
 		AcDbObjectPointer<AcDbBlockReference> pBlkRef (obId, AcDb::kForRead);
-		if (pBlkRef.openStatus() != Acad::eOk) {acutPrintf(ACRX_T("\nCannot open for read.\n")); continue;};
-		//AcDbObjectId idBlkTblRec = pBlkRef->blockTableRecord();
+		if (pBlkRef.openStatus() != Acad::eOk) {acutPrintf(ACRX_T("\nCannot open for read.\n")); delete pBlkRef; continue;};
 		AcDbDynBlockReference dynBlk(obId);
 		if (dynBlk.isDynamicBlock()) 
 			{
@@ -71,26 +60,40 @@ void selectBlocks()
 				if (pBTR.openStatus() == Acad::eOk)
 				{
 					const ACHAR *blkName = NULL; pBTR->getName(blkName);
-					acutPrintf(ACRX_T("\nDynamic block with name: \"%s\""), blkName);
-				} else {continue;}
+					wchar_t tmp[5];
+					//acutPrintf(ACRX_T("\nDEBUG blkName: %s"), blkName);
+					wcsncpy(tmp, blkName, 4);
+					wcscat(tmp, L"\0");
+					//acutPrintf(ACRX_T("\nDEBUG tmp: %s"), tmp);
+					if (wcscmp(tmp, L"Arm_") != 0) {
+						delete pBTR;
+						pBlkRef->close();
+						acutPrintf(ACRX_T("\nContinued."));
+						continue;
+					}
+					//acutPrintf(ACRX_T("\nDynamic block with name: \"%s\""), blkName);
+				} else {
+					delete pBTR;
+					pBlkRef->close(); 
+					continue;
+				}
 				AcDbDynBlockReferencePropertyArray blkPropAry;
 				dynBlk.getBlockProperties(blkPropAry);
-				Acad::ErrorStatus err;
+				//Acad::ErrorStatus err;
 				AcDbDynBlockReferenceProperty blkProp;
 				for(long lIndex=0L ; lIndex<blkPropAry.length() ; ++lIndex){
 					blkProp = blkPropAry[lIndex];
 					if (wcscmp(blkProp.propertyName().kACharPtr(), L"ÄËÈÍÀ") == 0){
-						acutPrintf(ACRX_T("\n%s : "),blkProp.propertyName());
+						//acutPrintf(ACRX_T("\n%s : "),blkProp.propertyName());
 					    AcDbEvalVariant currentValue = blkProp.value();
 					    //acutPrintf(ACRX_T("\n Restype %d"),currentValue.restype);
-					    acutPrintf(ACRX_T("%g\n"),currentValue.resval.rreal);
+					    acutPrintf(ACRX_T("\n%.0f"),currentValue.resval.rreal);
 					}
 				}
 				//http://arxdummies.blogspot.com/2005/03/class-6-entities.html
 				// First we will look into BlockReference entity looking for
 				// all non-constant attributes which are stored into block insertions
 				AcDbObjectIterator *pIter = pBlkRef->attributeIterator() ;
-				//acutPrintf("\nBlock's Non-constant Attributes:");
 				while ( !pIter->done () ) {
 					AcDbEntity *pAttEnt = NULL;
 					AcDbObjectId idAtt = pIter->objectId();
@@ -102,16 +105,23 @@ void selectBlocks()
 							//msg.Format("\nTAG:%s - VALUE:%s",pAtt->tag(),pAtt->textString());
 							//acutPrintf(msg);
 							if (wcscmp(pAtt->tag(), L"ÄÈÀÌ") == 0) {
-								acutPrintf(ACRX_T("\nTAG:%s - VALUE:%s"),pAtt->tag(),pAtt->textString());
+								//acutPrintf(ACRX_T("\nTAG:%s - VALUE:%s"),pAtt->tag(),pAtt->textString());
+								acutPrintf(ACRX_T(" %s:%s"),pAtt->tag(),pAtt->textString());
+							}
+							if (wcscmp(pAtt->tag(), L"ÏÎÇ") == 0) {
+								//acutPrintf(ACRX_T("\nTAG:%s - VALUE:%s"),pAtt->tag(),pAtt->textString());
+								acutPrintf(ACRX_T(" %s:%s"),pAtt->tag(),pAtt->textString());
 							}
 						}
 						pAttEnt->close();
 					}
 					pIter->step();
 				}
-				delete pIter ;				
+				delete pIter ;	
+				pBTR->close();
 			}
 		counter++;
+		pBlkRef->close();
 	}
 	acutPrintf(ACRX_T("\nThe counter is %d."), counter);
 	acedSSFree(ssname);
@@ -121,7 +131,6 @@ void selectBlocks()
 void runIt()
 {
 	selectBlocks();
-	//PrintBlockName();
 }
 
 void initApp()
